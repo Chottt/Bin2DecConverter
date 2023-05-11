7cf2311083841a2d3753f3679fd7f6d747ccc6e9
Chotipong Chansri <122771934+Chottt@users.noreply.github.com>
2023-05-11 13:02:04 +0700

    Update BinaryConverter.cpp

--- BinaryConverter.cpp
+++ BinaryConverter.cpp
@@ -3,9 +3,6 @@
 #include <iostream>
 #include <cmath>
 
-// for test I don't know that work. Hahaa
-
-
 using namespace std;
 
 struct DataBin
@@ -34,8 +31,7 @@ DataBin BinaryCalculate(long long number)
 
 int main()
 {
-    double number = 200.12345; // Enter your digit for convert
-    int bin2dec = 0;
+    int number = 10; // Enter your digit for convert
 
     long long* Ptr = (long long*) (&number);
 
@@ -47,10 +43,9 @@ int main()
     cout << "Binary: " << endl;
     for (int i = 0; i < (sizeof(double) * 8); ++i)
     {
-        bin2dec += ((dataBinary.dataBinary[i]) * pow(2, (i)));
+        cout << dataBinary.dataBinary[63-i];
     }
 
     cout << endl;
-    cout << bin2dec;
     return 0;
 }
