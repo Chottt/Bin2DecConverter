0aa1d5f22ee6279544de66c3bff75d933e988b90
Chotipong Chansri <122771934+Chottt@users.noreply.github.com>
2023-05-11 09:03:36 +0700

    Add files via upload

---
+++ BinaryConverter.cpp
@@ -0,0 +1,57 @@
+// BinaryCalTool.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+struct DataBin
+{
+    int dataBinary[64];
+};
+
+DataBin BinaryCalculate(long long number)
+{
+    long long filter = 1;
+    DataBin dataBinary;
+
+    for (int i = 0; i < (sizeof(number) * 8); ++i)
+    {
+        if (number & (filter << i))
+        {
+            dataBinary.dataBinary[i] = 1;
+        }
+        else
+        {
+            dataBinary.dataBinary[i] = 0;
+        }
+    }
+    return dataBinary;
+}
+
+int main()
+{
+    double number = 200.12345; // Enter your digit for convert
+    int bin2dec = 0;
+
+    long long* Ptr = (long long*) (&number);
+
+    auto dataBinary = BinaryCalculate(*Ptr);
+
+    cout << endl;
+    cout << "Your digit: " << number << endl;
+
+    cout << "Binary: " << endl;
+    for (int i = 0; i < (sizeof(double) * 8); ++i)
+    {
+        cout << dataBinary.dataBinary[63 - i] << " ";
+        if (i < 32)
+        {
+            bin2dec += ((dataBinary.dataBinary[i]) * pow(2, (i)));
+        }
+    }
+
+    cout << endl;
+    cout << bin2dec;
+    return 0;
+}
